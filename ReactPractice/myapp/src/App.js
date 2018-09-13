cb1b99d18c3ae5d8a99f14d980991e66a09d63c6
jbristol85 <josh.bristol2@gmail.com>
2018-09-13 18:26:51 -0400

    completed through video 14

---
+++ ReactPractice/myapp/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+class App extends Component {
+    state = {
+        ninjas: [
+            { name: "Josh", age: 32, belt: "black", id: 1 },
+            { name: "Jim", age: 31, belt: "blue", id: 2 },
+            { name: "Matt", age: 30, belt: "brown", id: 3 }
+        ]
+    }
+  render() {
+    return (
+      <div className="App">
+          <h1>My first React App!</h1>
+          <Ninjas ninjas= { this.state.ninjas } />
+
+      </div>
+    );  
+  }
+}
+
+export default App;
